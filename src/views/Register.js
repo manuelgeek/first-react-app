c6f5e774ee0e8a938a9b944dafba5705d4d76bdf
Magak Emmanuel <emashmagak@gmail.com>
2021-11-23 10:30:00 +0300

    useForm learning

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from '@hookform/error-message';
 
 function Register(props) {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  console.log(watch("name")); // watch input value by passing the name of it
+  watch((value, { name, type }) => console.log(value, name, type));
   return (
     <div>
       <br/>
-      <form action="">
-        <input type="text" name="" id="" placeholder={'name'}/> <br/><br/>
-        <input type="email" name="" id="" placeholder={'email'}/> <br/><br/>
-        <input type="password" name="" id="" placeholder={'password'}/> <br/><br/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name", {required: true})} type="text" name="name" id="name" placeholder={'name'}/>
+        {errors.name && <small><br/>This field is required</small> }
+        <ErrorMessage
+          errors={errors}
+          name="name"
+          render={({ message }) => <p>{message}</p>}
+        />
+        <br/><br/>
+        <input type="email" name="" id="email" placeholder={'email'}/> <br/><br/>
+        <input type="password" name="" id="password" placeholder={'password'}/> <br/><br/>
+        <input type="submit" name="" id=""/>
       </form>
     </div>
   );
